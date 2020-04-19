096179776e273e4f2345f37dfe06a940e19faad8
toddwbates <toddwbates@gmail.com>
2020-04-19 13:29:04 +0200

    baseline testing
    With no test written there should be 0% coverage. This sets up the project with code coverage.

---
+++ UnitTestHostApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTestHostApp
+//
+//  Created by Todd Bates on 4/19/20.
+//  Copyright © 2020 objc.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
